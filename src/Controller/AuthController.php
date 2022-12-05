8f2490ce7eea31db1b43bd87d9ce4f168a246478
Emre Koc <hi@emrekoc.io>
2022-12-05 10:15:02 +0100

    Add authentication

---
+++ src/Controller/AuthController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class AuthController extends AbstractController
+{
+    #[Route('/login', name: 'auth_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('auth/login.html.twig', [
+            'error'         => $error,
+            'last_username' => $lastUsername,
+        ]);
+    }
+
+    #[Route('/logout', name: 'auth_logout', methods: ['GET'])]
+    public function logout()
+    {
+        return null;
+    }
+}
