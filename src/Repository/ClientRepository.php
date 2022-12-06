48881ff26eecd9a077b54a4391c93a5d9278a84e
Emre Koc <hi@emrekoc.io>
2022-12-06 10:15:01 +0100

    Order Client findAll by name

--- src/Repository/ClientRepository.php
+++ src/Repository/ClientRepository.php
@@ -39,28 +39,12 @@ class ClientRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Client[] Returns an array of Client objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('c.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function findAll()
+    {
+        $orderBy = [
+            'name' => 'ASC',
+        ];
 
-//    public function findOneBySomeField($value): ?Client
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+        return $this->findBy([], $orderBy);
+    }
 }
