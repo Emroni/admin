66ab16f92f3093ad2ac851c17e7dda3f0308b1ab
Emre Koc <hi@emrekoc.io>
2022-12-06 11:26:56 +0100

    Add Client edit form

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\Type\ClientType;
 use App\Repository\ClientRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -34,6 +36,28 @@ class ClientController extends AbstractController
 
         return $this->render('client/view.html.twig', [
             'client' => $client,
+        ]);
+    }
+
+    #[Route('/client/{id}/edit', name: 'client_edit')]
+    public function edit(int $id, Request $request): Response
+    {
+        $client = $this->clientRepository->find($id);
+
+        $form = $this->createForm(ClientType::class, $client);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $client = $form->getData();
+            $this->clientRepository->save($client, true);
+            
+            return $this->redirectToRoute('client_view', [
+                'id' => $id,
+            ]);
+        }
+
+        return $this->render('client/edit.html.twig', [
+            'client' => $client,
+            'form' => $form,
         ]);
     }
 }
---
+++ src/Form/Type/ClientType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form\Type;
+
+use App\Entity\Client;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ClientType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('email', EmailType::class, [
+                'required' => false,
+            ])
+            ->add('address', TextareaType::class, [
+                'attr' => [
+                    'rows' => 3,
+                ],
+                'required' => false,
+            ])
+            ->add('save', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Client::class,
+        ]);
+    }
+
+}
