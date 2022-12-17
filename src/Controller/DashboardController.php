b4af8d58fddfa8cb84d67243d3333c17a6b07ce6
Emre Koc <hi@emrekoc.io>
2022-12-17 12:17:53 +0100

    Add billable Invoices to Dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Invoice;
 use App\Repository\InvoiceRepository;
+use App\Repository\TimeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,18 +14,42 @@ class DashboardController extends AbstractController
     /** @var InvoiceRepository */
     private $invoiceRepository;
 
-    public function __construct(InvoiceRepository $invoiceRepository)
+    /** @var TimeRepository */
+    private $timeRepository;
+
+    public function __construct(InvoiceRepository $invoiceRepository, TimeRepository $timeRepository)
     {
         $this->invoiceRepository = $invoiceRepository;
+        $this->timeRepository = $timeRepository;
     }
 
     #[Route('/', name: 'dashboard')]
     public function index(): Response
     {
+        // Get awaiting invoices
         $awaitingInvoices = $this->invoiceRepository->findAwaiting();
+
+        // Get billable invoices
+        $billableInvoices = [];
+        foreach ($this->timeRepository->findBillable() as $time) {
+            $client = $time->getTask()->getProject()->getClient();
+            $clientId = $client->getId();
+
+            if (!isset($billableInvoices[$clientId])) {
+                $invoice = new Invoice();
+                $invoice->setClient($client);
+                $invoice->updateType();
+                $billableInvoices[$clientId] = $invoice;
+            }
+
+            $invoice = $billableInvoices[$clientId];
+            $invoice->addTime($time);
+            $invoice->updateAmount();
+        }
 
         return $this->render('dashboard/index.html.twig', [
             'awaitingInvoices' => $awaitingInvoices,
+            'billableInvoices' => $billableInvoices,
         ]);
     }
 }
